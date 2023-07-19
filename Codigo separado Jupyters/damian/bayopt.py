07f54920c1b2c075a13a0aff43cade3158a5d441
Damian Piuselli <damianpiuselli@gmail.com>
2023-07-19 16:26:17 -0300

    agrego funciones de adq

--- Codigo separado Jupyters/damian/bayopt.py
+++ Codigo separado Jupyters/damian/bayopt.py
@@ -3,6 +3,76 @@ from sklearn.gaussian_process import GaussianProcessRegressor
 from sklearn.gaussian_process.kernels import RBF
 from scipy.optimize import minimize
 from scipy.spatial.distance import pdist
+from scipy.stats import norm
+
+# Funciones de adquisicion.
+# Fuente: https://ekamperi.github.io/machine%20learning/2021/06/11/acquisition-functions.html
+
+
+def EI(x, gp, y_max, epsilon=0):
+    """funcion de adquisicion - expected improvement
+
+    Args:
+        x (np.array): valor de prueba en forma de vector.
+        gp (GaussianProcessRegressor): Modelo
+        y_max (float): valor maximo del set de entrenamiento.
+        epsilon (int, optional): valores positivos favorecen exploracion vs explotacion, default=0.
+
+    Returns:
+        float: expecativa de mejora
+    """
+
+    # Calcular media y desvio para el valor de prueba "x"
+    mean, std = gp.predict(x, return_std=True)
+
+    # calcular y devolver la expectativa de mejora
+    a = mean - y_max - epsilon
+    z = a / std
+
+    return a * norm.cdf(z) + std * norm.pdf(z)
+
+
+def PI(x, gp, y_max, epsilon=0):
+    """funcion de adquisicion - probability of improvement
+
+    Args:
+        x (np.array): valor de prueba en forma de vector.
+        gp (GaussianProcessRegressor): Modelo
+        y_max (float): valor maximo del set de entrenamiento.
+        epsilon (int, optional): valores positivos favorecen exploracion vs explotacion, default=0.
+
+    Returns:
+        float: probabilidad de mejora
+    """
+
+    # Calcular media y desvio para el valor de prueba "x"
+    mean, std = gp.predict(x, return_std=True)
+
+    # calcular y devolver la probabilidad de mejora
+
+    z = mean - y_max - epsilon / std
+
+    return norm.cdf(z)
+
+
+def UCB(x, gp, kappa=1.96):
+    """funcion de adquisicion - upper confidence bound
+
+    Args:
+        x (np.array): valor de prueba en forma de vector.
+        gp (GaussianProcessRegressor): Modelo
+        kappa (int, optional): mayores valores favorecen exploracion vs explotacion, default=1.
+
+    Returns:
+        float: mean + kappa * std
+    """
+
+    # Calcular media y desvio para el valor de prueba "x"
+    mean, std = gp.predict(x, return_std=True)
+
+    # calcular y devolver la probabilidad de mejora
+
+    return mean + kappa * std
 
 
 class BayOptRBF:
@@ -10,7 +80,7 @@ class BayOptRBF:
         # Guardo los datos de entrenamiento/ parametros
         self.X_train = X_train
         self.y_train = y_train
-        self.alpha = alpha  # varianza de ruido gaussiano adicional a las mediciones
+        self.alpha = alpha  # varianza del ruido gaussiano adicionado a los datos
 
         # Inicializo el kernel
         self.kernel = (
@@ -26,3 +96,5 @@ class BayOptRBF:
         )
         self.regressor.fit(self.X_train, self.y_train)
 
+    def global_max(self, acq_fun="EI", n_restarts=20):
+        pass
