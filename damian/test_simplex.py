7be58fcb8e3e4d5a9ec6a6d82074ed70253586d1
DamianPiuselli <damianpiuselli@gmail.com>
2023-08-23 17:23:39 -0300

    testeo de simplex ok

--- damian/test_simplex.py
+++ damian/test_simplex.py
@@ -19,7 +19,21 @@ if __name__ == "__main__":
 
     # model
     # print(x_data, y_data)
-    model = SimplexOpt(bounds=bounds, step=1)
-    print(model.current_vertex)
-    model.simplex(model.current_vertex, [1, 2, 3])
-    print(model.current_vertex)
+    model = SimplexOpt(bounds=bounds, step=0.1)
+
+    # opt
+
+    pct_max = []
+    tested_values = []
+    for iteration in range(20):
+        x_vertex_data = model.current_vertex
+        y_vertex_data = [RS(x) for x in x_vertex_data]
+
+        x_next = model.simplex(x_vertex_data, y_vertex_data)
+        y_next = RS(x_next)
+
+        pct_max.append(y_next)
+        tested_values.append(x_next)
+
+    for index, value in enumerate(pct_max):
+        print(f"n={index}, pct_max={100*value}, X= {tested_values[index]}")
