bd15eeb29b569aab5ef52bcbee5599312d7ecd19
DamianPiuselli <damianpiuselli@gmail.com>
2023-08-24 13:45:37 -0300

    testeo de dos estrategias

--- damian/comparaciones.py
+++ damian/comparaciones.py
@@ -1,10 +1,14 @@
 import numpy as np
-from bayopt import SimplexOpt, RandomQuadratic
+from bayopt import BayOptRBF, RandomQuadratic
+import warnings
 import itertools
+
+warnings.filterwarnings("ignore")
+
 
 if __name__ == "__main__":
     # vars
-    n_dim = 3
+    n_dim = 10
 
     _1d_bound = [-1, 1]
 
@@ -19,21 +23,23 @@ if __name__ == "__main__":
 
     # model
     # print(x_data, y_data)
-    model = SimplexOpt(bounds=bounds, step=0.1)
+    model = BayOptRBF(x_data, y_data)
 
-    # opt
-
+    # optimizacion
     pct_max = []
     tested_values = []
-    for iteration in range(20):
-        x_vertex_data = model.current_vertex
-        y_vertex_data = [RS(x) for x in x_vertex_data]
-
-        x_next = model.simplex(x_vertex_data, y_vertex_data)
+    for iteration in range(10):
+        x_next, acq_func_val = model.global_max(acq_fun="EI", bounds=bounds)
+        # valor de la funcion en el punto encontrado
+        # como la funcion esta normalizada entre 0 y 1, tmb es la fraccion del maximo.
         y_next = RS(x_next)
-
         pct_max.append(y_next)
         tested_values.append(x_next)
+        # agrego el nuevo punto a los datos para entrenar el modelo
+        x_data = np.concatenate((x_data, [x_next]))
+        y_data = np.concatenate((y_data, [y_next]))
+        # fiteo el model
+        model.fit(x_data, y_data)
 
     for index, value in enumerate(pct_max):
         print(f"n={index}, pct_max={100*value}, X= {tested_values[index]}")
